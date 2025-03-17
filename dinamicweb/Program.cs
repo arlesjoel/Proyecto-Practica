7ffed8c1e7728af69aaca426fd85004a38928ecd
arlesjoel <arlesjoelgs1@gmail.com>
2025-03-16 22:23:54 -0600

    iniciando repositorio

---
+++ dinamicweb/Program.cs
@@ -0,0 +1,25 @@
+using dinamicweb.Components;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
